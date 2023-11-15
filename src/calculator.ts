c619c94f76c97c276dc6942537fb9e452ac8415c
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-11-15 19:17:32 -0300

    refine calculator tests

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,17 +1,20 @@
 const calculator = {
-    add: (num1: number, num2: number): number => {
+    add: (num1: number, num2: number = 0): number => {
         return num1 + num2;
     },
-    subtract: (num1: number, num2: number): number => {
+
+    subtract: (num1: number, num2: number = 0): number => {
         return num1 - num2;
     },
-    divide: (num1: number, num2: number): number => {
+
+    divide: (num1: number, num2: number = 1): number => {
         if (num2 === 0) {
             throw new Error("Cannot divide by zero");
         }
         return num1 / num2;
     },
-    multiply: (num1: number, num2: number): number => {
+
+    multiply: (num1: number, num2: number = 1): number => {
         return num1 * num2;
     },
 };
--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -2,29 +2,44 @@ import { describe, test, expect } from "vitest";
 import { calculator } from "../calculator";
 
 describe("Calculator will", () => {
-    test("Correctly add two numbers", () => {
+    test("correctly add two numbers", () => {
         const result = calculator.add(2, 3);
         expect(result).toBe(5);
     });
 
-    test("Correctly subtract two numbers", () => {
+    test("correctly add two negative numbers", () => {
+        const result = calculator.add(-15, -35);
+        expect(result).toBe(-50);
+    });
+
+    test("correctly subtract two numbers", () => {
         const result = calculator.subtract(5, 3);
         expect(result).toBe(2);
     });
 
-    test("Correctly divide two numbers", () => {
+    test("correctly subtract two negative numbers", () => {
+        const result = calculator.subtract(-15, -40);
+        expect(result).toBe(25);
+    });
+
+    test("correctly divide two numbers", () => {
         const result = calculator.divide(6, 2);
         expect(result).toBe(3);
     });
 
-    test("Throw an error when dividing by zero", () => {
+    test("throw an error when dividing by zero", () => {
         expect(() => calculator.divide(6, 0)).toThrow("Cannot divide by zero");
     });
 
-    test("Correctly multiply two numbers", () => {
+    test("correctly multiply two numbers", () => {
         const result = calculator.multiply(2, 3);
         expect(result).toBe(6);
     });
 
-
+    test("handle just one argument", () => {
+        expect(calculator.add(5)).toBe(5);
+        expect(calculator.subtract(10)).toBe(10);
+        expect(calculator.multiply(15)).toBe(15);
+        expect(calculator.divide(30)).toBe(30);
+    });
 });
