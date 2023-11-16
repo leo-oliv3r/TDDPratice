e806b9d9bf022bc79b958bf10d5f03fe156f2539
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-11-15 23:06:16 -0300

    Add comment to better explain shiftChar function

--- src/caesarCipher.ts
+++ src/caesarCipher.ts
@@ -6,11 +6,20 @@ const shiftChar = (char: string, key: number) => {
 
     if (/[A-Z]/.test(char)) {
         // 65 is "A" in ASCII
+        /*
+        This next line subtract the currentCharCode from the "base" letter A
+        to get the the correct "position" of the letter on the alphatet
+        A would be 0, B would be 1 and so forth
+        Add the key value and do modulus 26 to keep the number within the bounds
+        of the 26 letter alphabet. Add the "base" letter back to get the
+        correct ASCII code of the shifted character
+        */
         const shiftedCharCode = ((currentCharCode - 65 + key) % 26) + 65;
         return String.fromCharCode(shiftedCharCode);
     }
 
     // 97 is "a" in ASCII
+    // Does the same as the explanation above
     const shiftedCharCode = ((currentCharCode - 97 + key) % 26) + 97;
     return String.fromCharCode(shiftedCharCode);
 };
@@ -19,7 +28,6 @@ export function caesarCipher(text: string, nrOfShifts: number = 1) {
     if (nrOfShifts < 0) {
         throw new Error("nrOfShifts can't be less than zero");
     }
-
     if (nrOfShifts === 0) return text;
 
     nrOfShifts = Math.round(nrOfShifts);
