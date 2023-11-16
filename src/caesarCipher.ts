58f8193c72b1055beb139dc995c30e8f8a5a168a
leonardo-vic3nte <leonardo.vic3nte@gmail.com>
2023-11-15 22:55:41 -0300

    Add caesarCipher and its test suit

--- src/caesarCipher.ts
+++ src/caesarCipher.ts
@@ -1,10 +1,37 @@
-export function caesarCipher(text: string, nrOfShifts: number) {
+const isLetter = (char: string) => /[a-zA-Z]/.test(char);
 
-    if(nrOfShifts < 1){
-        throw new Error("nrOfShifts can't be less than zero");
-        
+const shiftChar = (char: string, key: number) => {
+    // Not possible to be undefined in this case
+    const currentCharCode = char.codePointAt(0) as number;
+
+    if (/[A-Z]/.test(char)) {
+        // 65 is "A" in ASCII
+        const shiftedCharCode = ((currentCharCode - 65 + key) % 26) + 65;
+        return String.fromCharCode(shiftedCharCode);
     }
 
+    // 97 is "a" in ASCII
+    const shiftedCharCode = ((currentCharCode - 97 + key) % 26) + 97;
+    return String.fromCharCode(shiftedCharCode);
+};
 
+export function caesarCipher(text: string, nrOfShifts: number = 1) {
+    if (nrOfShifts < 0) {
+        throw new Error("nrOfShifts can't be less than zero");
+    }
+
+    if (nrOfShifts === 0) return text;
+
+    nrOfShifts = Math.round(nrOfShifts);
+
+    let shiftedText: string[] = [];
+    text.split("").forEach((char) => {
+        if (isLetter(char)) {
+            shiftedText.push(shiftChar(char, nrOfShifts));
+        } else {
+            shiftedText.push(char);
+        }
+    });
+
+    return shiftedText.join("");
 }
-
--- src/tests/caesarCipher.test.ts
+++ src/tests/caesarCipher.test.ts
@@ -1,20 +1,44 @@
 import { describe, test, expect } from "vitest";
 import { caesarCipher } from "../caesarCipher";
 
-/*
- TESTS LIST
- ----------
- - Will correctly decipher a message given a string and a nrOfShifts argument
- - Will shift the letters by 1 if no nrOfShifts argument is given
- - Will throw if a negative number is passed as nrOfShifts argument
- - Will return the same string if 0 is passed as nrOfShifts argument
- - Will correctly wrap the letters
- - Will correctly ignore anything other than letters
- - The returned message should preserve case
-*/
-
 describe("caesarCipher will", () => {
     test("throw if negative number is passed as nrOfShifts argument", () => {
         expect(() => caesarCipher("A phrase", -1)).toThrow("nrOfShifts can't be less than zero");
+    });
+
+    test("return the same string if 0 is passed as nrOfShifts argument", () => {
+        expect(caesarCipher("Some text", 0)).toBe("Some text");
+    });
+
+    test("nrOfShifts defaults to 1 if just one argument is passed", () => {
+        expect(caesarCipher("Another text")).toBe("Bopuifs ufyu");
+    });
+
+    test("round nrOfShits with pass a non integet", () => {
+        expect(caesarCipher("Another text", 1.3)).toBe("Bopuifs ufyu");
+    });
+
+    test("will ignore chars that are not letters", () => {
+        expect(caesarCipher("zAc12!#", 1)).toBe("aBd12!#");
+    });
+
+    test("will respect case", () => {
+        expect(caesarCipher("AbCdEzD", 1)).toBe("BcDeFaE");
+    });
+
+    test("will correctly wrap the letters around the alphabet", () => {
+        expect(caesarCipher("XYZ", 3)).toBe("ABC");
+    });
+
+    test("will correctly shift the letters N times", () => {
+        expect(caesarCipher("This is a sentence", 16)).toBe("Jxyi yi q iudjudsu");
+    });
+
+    test("will correctly shift the letters N times", () => {
+        expect(caesarCipher("This is a sentence", 183)).toBe("Uijt jt b tfoufodf");
+    });
+
+    test("preserves spaces and punctuation", () => {
+        expect(caesarCipher("Hello, World!", 1)).toBe("Ifmmp, Xpsme!");
     });
 });
